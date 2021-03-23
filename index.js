a0a0ce5be61dde35f7dd244b9d0440ce867ef5b5
m-a-hossain <adam.eve007@gmail.com>
2021-03-24 02:30:06 +0600

    just working to learn

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 
 
 const app =express()
-
+app.use(cors())
+app.use(bodyParser.json())
+const users=["aaron",'suhel','abid','adhin','azoa','manha'];
 const rootCall=(req,res)=>{
-res.send('welcome to server side')
+res.send('welcome to server side now node mon')
 }
 
 app.get('/',(req,res)=>{
-    res.send('thank for calling me!!')
+    const fruit={
+        product:'orange',
+        price:222,
+        color:'orangeColor'
+    }
+    res.send(fruit)
 })
+//post
+app.post('/adduser',(req,res)=>{
+    console.log(req.body)
+})
+app.get('/fruit/banana',(req,res)=>{
+    res.send({fruitName:'banana',quantity:10,price:1000})
+})
+
+app.get('/users/:id', (req,res)=>{
+    const id= req.params.id 
+    const name =users[id]
+    res.send({id,name})
+})
+
+
 app.listen(5000,()=> console.log('listening to the port of 5000'))
